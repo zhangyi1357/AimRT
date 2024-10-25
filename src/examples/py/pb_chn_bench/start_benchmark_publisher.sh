4a22be8194c47c28acd1c75ce6295ad2e68d81cd
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2024-10-25 17:53:30 +0800

    feat: enhance benchmark configuration for ROS2 support
    Extend the configuration for both publisher and subscriber modules to integrate ROS2 as the communication backend, improving flexibility and enabling more robust messaging patterns. Adjust executor and logging settings to optimize performance in the new setup.

--- src/examples/py/pb_chn_bench/start_benchmark_publisher.sh
+++ src/examples/py/pb_chn_bench/start_benchmark_publisher.sh
@@ -2,4 +2,6 @@
 
 export AIMRT_PLUGIN_DIR=$(pip show aimrt_py | grep Location | awk '{print $2}')/aimrt_py
 
+source ${AIMRT_PLUGIN_DIR}/share/ros2_plugin_proto/local_setup.bash
+
 python3 benchmark_publisher_app.py --cfg_file_path ./cfg/benchmark_publisher_cfg.yaml
